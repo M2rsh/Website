73948bd6be723b5cac3f814e46ff39fcfbf8b3a4
M2rsh <mariuszbubak10@gmail.com>
2022-07-28 18:18:31 +0200

    Package update

---
+++ pages/404.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/Error.module.scss";
+
+//*<meta content="https://embed.com/embedimage.png" property="og:image"/>*//
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>404 Not Found</title>
+        <meta name="description" content="404 Not Found" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta content="404 Not Found" property="og:title"/>
+        <meta content="Page not found." property="og:description"/>
+        <meta content="https://m2rsh-beta.netlify.app/" property="og:url"/>
+        <meta content="#b54842" data-react-helmet="true" name="theme-color"/>
+      </Head>
+      <div className={styles.container}>
+        <p>Penis</p>
+      </div>
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
+function M2rshWebsite({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default M2rshWebsite
