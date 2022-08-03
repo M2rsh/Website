c215c79a27c346d243205700da3ab5dd2e5ff2f9
M2rsh <mariuszbubak10@gmail.com>
2022-08-03 02:35:24 +0200

    lowercase

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req: any, res: any) {
-  if (req.query.name === "coffee") {
+  if ((req.query.name).toLowerCase === "coffee") {
     res.status(418).json({ error: 418, message: "I'm a teapot" });
   } else {
     res.status(200).json({ name: req.query.name ? req.query.name : "World" });
