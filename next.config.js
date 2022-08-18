b1bed4bd6af218aeb4f6875b2101d67f35719a1f
M2rsh <mariuszbubak10@gmail.com>
2022-08-18 17:43:53 +0200

    redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/bot-invite",
+        destination:
+          "https://discord.com/api/oauth2/authorize?client_id=969292707019247717&permissions=274878220352&scope=bot%20applications.commands",
+        permanent: true,
+      },
+    ];
+  },
+  reactStrictMode: true,
+};
