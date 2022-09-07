9174cbf7f0b495e82e94580af41234baf366b459
M2rsh <mariuszbubak10@gmail.com>
2022-09-07 21:18:16 +0200

    Removed funny h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,6 @@ export default class Home extends React.Component {
         </Head>
         <Header />
         <div className={styles.container}>
-          <h1>Penis 🤣🤣🤣</h1>
           <Image src={lizzyGif} alt="lizzy" width={1200} height={480} className="image" unoptimized={true}/>
         </div>
         <Footer />
