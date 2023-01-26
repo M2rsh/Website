7e7e1a341657b74ac0a70fd2b1d297530c17f50b
M2rsh <mariuszbubak10@gmail.com>
2023-01-26 19:17:05 +0100

    Update

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,7 +4,7 @@ import { HttpStatusCode } from "solid-start/server";
 export default function NotFound() {
   return (
     <main>
-      <Title>Not Found</Title>
+      <Title>404 Not Found</Title>
       <HttpStatusCode code={404} />
       <h1>Page Not Found</h1>
       <p>
